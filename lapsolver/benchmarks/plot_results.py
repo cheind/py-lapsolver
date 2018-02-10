5d3ff9e496f62c1352c9b3da8ca75f6a3f998d96
Christoph Heindl <christoph.heindl@gmail.com>
2018-02-10 19:05:04 +0100

    adding benchmark code

---
+++ lapsolver/benchmarks/plot_results.py
@@ -0,0 +1,56 @@
+import argparse
+import json
+import re
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Plot benchmark results.', formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('benchmarkfile', type=str, help='Json file containing benchmark results')   
+    #parser.add_argument('tests', type=str, help='Directory containing tracker result files')
+    #parser.add_argument('--loglevel', type=str, help='Log level', default='info')
+    #parser.add_argument('--fmt', type=str, help='Data format', default='mot15-2D')
+    #parser.add_argument('--solver', type=str, help='LAP solver to use')
+    return parser.parse_args()
+
+def build_dataframe(args):
+    dre = re.compile(r"'(.*)'")
+    
+    with open(args.benchmarkfile) as f:
+        data = json.load(f)
+
+    events = []
+    for b in data['benchmarks']:
+        ei = b['extra_info']
+        dtype = dre.search(ei['scalar']).group(1)
+        events.append(('{}x{}'.format(ei['size'][0], ei['size'][1]), ei['solver'], dtype, b['stats']['mean'], b['stats']['stddev']))
+
+    return pd.DataFrame(events, columns=['matrix-size', 'solver', 'scalar', 'mean-time', 'stddev'])
+
+def draw_plots(df):
+    sns.set_style("whitegrid")
+    for s,g in df.groupby('scalar'):
+        title='Benchmark results for dtype={}'.format(s)
+        ax = sns.barplot(x='mean-time', y='matrix-size', hue='solver', data=df, errwidth=0, palette="muted")
+        ax.set_xscale("log")
+        ax.set_xlabel('mean-time (sec)')
+        plt.title(title)
+        plt.tight_layout()
+        plt.savefig('benchmark-dtype-{}.png'.format(s), transparent=True, )
+        plt.show()
+
+
+def main():
+    print('here')
+    args = parse_args()
+
+    df = build_dataframe(args)
+    draw_plots(df)
+    print(df)
+    
+
+if __name__ == '__main__':
+    main()
+    
--- lapsolver/benchmarks/test_benchmark_solvers.py
+++ lapsolver/benchmarks/test_benchmark_solvers.py
@@ -2,6 +2,7 @@ from scipy.optimize import linear_sum_assignment
 import numpy as np
 import pytest
 import importlib
+import sys
 
 """
 
@@ -28,19 +29,29 @@ def test_bench_lapsolver(benchmark, data):
 
 def load_solver_lapsolver():
     from lapsolver import solve_dense
-    return solve_dense
+
+    def run(costs):
+        rids, cids = solve_dense(costs)
+        return costs[rids, cids].sum()
+        
+    return run
 
 def load_solver_scipy():
     from scipy.optimize import linear_sum_assignment
-    return linear_sum_assignment
+
+    def run(costs):
+        rids, cids = linear_sum_assignment(costs)
+        return costs[rids, cids].sum()
+
+    return run
 
 def load_solver_munkres():
     from munkres import Munkres, DISALLOWED
     
     def run(costs):
         m = Munkres()
-        indices = np.array(m.compute(costs), dtype=int)
-        return indices[:,0], indices[:,1]
+        idx = np.array(m.compute(costs), dtype=int)
+        return costs[idx[:,0], idx[:,1]].sum()
     
     return run
 
@@ -48,17 +59,37 @@ def load_solver_lapjv():
     from lap import lapjv
 
     def run(costs):
-        r = lapjv(costs, return_cost=True)
-        return range(costs.shape[0]), r[1]
+        r = lapjv(costs, return_cost=True, extend_cost=True)
+        return r[0]
     
+    return run
+
+def load_solver_ortools():
+    from ortools.graph import pywrapgraph
+
+    def run(costs):
+        f = 1e3
+        valid = np.isfinite(costs)
+        # A lot of time in ortools is being spent in constructing the graph.
+        assignment = pywrapgraph.LinearSumAssignment()
+        for r in range(costs.shape[0]):
+            for c in range(costs.shape[1]):
+                if valid[r,c]:
+                    assignment.AddArcWithCost(r, c, int(costs[r,c]*f))
+        
+        # No error checking for now
+        assignment.Solve()
+        return assignment.OptimalCost() / f
+
     return run
 
 def load_solvers():
     loaders = [
         ('lapsolver', load_solver_lapsolver),
-        #('scipy', load_solver_scipy),
-        #('munkres', load_solver_munkres),
         ('lapjv', load_solver_lapjv),
+        ('scipy', load_solver_scipy),
+        ('munkres', load_solver_munkres),        
+        ('ortools', load_solver_ortools),
     ]
 
     solvers = {}
@@ -71,16 +102,57 @@ def load_solvers():
 
 
 solvers = load_solvers()
+sizes = [
+    ([10,10], -80040), 
+    ([10,5], -80040), 
+    ([20,20], -175988), 
+    ([50,20], -467118),
+    ([50,50], -467118),    
+    ([100,100], 0),
+    ([200,200], 0),
+    ([500,500], 0),
+    ([1000,1000], 0),
+    ([5000,5000], 0),
+]
+size_max = [5000,5000]
 
+np.random.seed(123)
+icosts = np.random.randint(-1e4, 1e4, size=size_max)
 
+@pytest.mark.benchmark(
+    min_time=1,
+    min_rounds=2,
+    disable_gc=False,
+    warmup=True,
+    warmup_iterations=1    
+)
 @pytest.mark.parametrize('solver', solvers.keys())
-@pytest.mark.parametrize('size', [[10,10], [100,100], [1000,1000], [5000,5000]])
-#@pytest.mark.timeout(20)
-def test_benchmark_solver(benchmark, solver, size):
+@pytest.mark.parametrize('scalar', [int, np.float32])
+@pytest.mark.parametrize('size,expected', sizes)
 
-    np.random.seed(123)
-    #costs = np.random.randint(-10000, 10000, size=size)
-    costs = np.random.uniform(-100, 100, size=size)
-    r = benchmark(solvers[solver], costs.copy())
-    #print(costs[r[0], r[1]].sum())
+def test_benchmark_solver(benchmark, solver, scalar, size, expected):
 
+    exclude_above = {
+        'munkres' : 200,
+        'scipy' : 1000,
+        'ortools' : 5000
+    }    
+
+    benchmark.extra_info = {
+        'solver': solver,
+        'size': size,
+        'scalar': str(scalar)
+    }
+
+    s = np.array(size)
+    if (s > exclude_above.get(solver, sys.maxsize)).any():
+        benchmark.extra_info['success'] = False
+        return
+
+    costs = icosts[:size[0], :size[1]].astype(scalar).copy()
+    r = benchmark(solvers[solver], costs)
+    print(solver, size, r)
+    if r != expected:
+        benchmark.extra_info['success'] = False
+
+# pytest lapsolver -k test_benchmark_solver -v --benchmark-group-by=param:size,param:scalar -s --benchmark-save=bench
