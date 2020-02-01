2e712fb079241aac77bf7cfd2e89f917b86009ea
Jack Valmadre <valmadre@google.com>
2020-02-01 12:55:29 +0100

    Use size strings to make test output clearer
    This also makes it easier to choose a subset of tests.

--- lapsolver/benchmarks/test_benchmark_solvers.py
+++ lapsolver/benchmarks/test_benchmark_solvers.py
@@ -1,3 +1,4 @@
+import collections
 from scipy.optimize import linear_sum_assignment
 import numpy as np
 import pytest
@@ -79,22 +80,23 @@ def load_solvers():
 
 
 solvers = load_solvers()
-sizes = [
-    ([10,5], -39518.0),
-    ([10,10], -80040.0),
-    ([20,20], -175988.0),
-    ([50,20], -193922.0),
-    ([50,50], -467118.0),
-    ([100,100], -970558.0),
-    ([200,200], -1967491.0),
-    ([500,500], -4968156.0),
-    ([1000,1000], -9968874.0),
-    ([5000,5000], -49969853.0),
-]
+size_to_expected = collections.OrderedDict([
+    ('10x5', -39518.0),
+    ('10x10', -80040.0),
+    ('20x20', -175988.0),
+    ('50x20', -193922.0),
+    ('50x50', -467118.0),
+    ('100x100', -970558.0),
+    ('200x200', -1967491.0),
+    ('500x500', -4968156.0),
+    ('1000x1000', -9968874.0),
+    ('5000x5000', -49969853.0),
+])
 size_max = [5000,5000]
 
 np.random.seed(123)
 icosts = np.random.randint(-1e4, 1e4, size=size_max)
+
 
 @pytest.mark.benchmark(
     min_time=1,
@@ -105,9 +107,11 @@ icosts = np.random.randint(-1e4, 1e4, size=size_max)
 )
 @pytest.mark.parametrize('solver', solvers.keys())
 @pytest.mark.parametrize('scalar', [int, np.float32])
-@pytest.mark.parametrize('size,expected', sizes)
+@pytest.mark.parametrize('size', [k for k, v in size_to_expected.items()])
 
-def test_benchmark_solver(benchmark, solver, scalar, size, expected):
+def test_benchmark_solver(benchmark, solver, scalar, size):
+    dims = _parse_size(size)
+    expected = size_to_expected[size]
 
     exclude_above = {
         'munkres' : 200,
@@ -120,14 +124,21 @@ def test_benchmark_solver(benchmark, solver, scalar, size, expected):
         'scalar': str(scalar)
     }
 
-    s = np.array(size)
+    s = np.array(dims)
     if (s > exclude_above.get(solver, sys.maxsize)).any():
         benchmark.extra_info['success'] = False
         return
 
-    costs = icosts[:size[0], :size[1]].astype(scalar).copy()
+    costs = icosts[:dims[0], :dims[1]].astype(scalar).copy()
     r = benchmark(solvers[solver], costs)
     if r != expected:
         benchmark.extra_info['success'] = False
+
+
+def _parse_size(size_str):
+    """Parses a string of the form 'MxN'."""
+    m, n = (int(x) for x in size_str.split('x'))
+    return m, n
+
 
 # pytest lapsolver -k test_benchmark_solver -v --benchmark-group-by=param:size,param:scalar -s --benchmark-save=bench
